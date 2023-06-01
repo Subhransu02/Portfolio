9d666965d9cb5e4b9c237d6fe0276a3a68a9495e
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2023-06-01 12:28:07 +0530

    changed react

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+
+import  ReactDOM from 'react-dom';
 import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "./Context/theme";
